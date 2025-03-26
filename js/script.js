1e277047bd16c722b0e2a5be9f8c6b1ddc6c0a59
keivega0789 <kei.s0789.180@gmail.com>
2025-03-26 14:43:37 +0900

    文言修正
    common
    ・電話番号修正
    
    service
    ・機械費追加
    ・ハウスクリーニング価格修正
    ・薪割り価格設定

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
+// ページロード時の処理
 window.addEventListener('load', () => {
-	setTimeout(() => {
-		document.body.classList.remove('fadeout');
-	}, 800); // 0.8秒（800ミリ秒）の遅延
+	setTimeout(() => document.body.classList.remove('fadeout'), 800); // 0.8秒の遅延
 });
 
-$(() => {
+// DOMコンテンツがロードされた後の処理
+document.addEventListener('DOMContentLoaded', () => {
 	// ハッシュリンク(#)と別ウィンドウで開くリンクをスルー
-	$('a:not([href^="#"]):not([target])').on('click', (e) => {
-		e.preventDefault(); // ナビゲートをキャンセル
-		const url = $(e.currentTarget).attr('href'); // 遷移先のURLを取得
-		if (url) {
-			$('body').addClass('fadeout'); // bodyに class="fadeout"を挿入
-			setTimeout(() => {
-				window.location = url; // 0.8秒後に取得したURLに遷移
-			}, 800);
-		}
-	});
+	document
+		.querySelectorAll('a:not([href^="#"]):not([target])')
+		.forEach((link) =>
+			link.addEventListener('click', (e) => {
+				e.preventDefault(); // ナビゲートをキャンセル
+				const url = link.getAttribute('href'); // 遷移先のURLを取得
+				if (url) {
+					document.body.classList.add('fadeout'); // bodyに "fadeout" クラスを追加
+					setTimeout(() => (window.location = url), 800); // 0.8秒後に遷移
+				}
+			})
+		);
 });
